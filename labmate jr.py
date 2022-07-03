0178831e223052883a5ee5b1440ce021d5bd615a
blaQ <f3mioloko@gmail.com>
2022-07-03 23:48:24 +0100

    qc frame2

--- labmate jr.py
+++ labmate jr.py
@@ -1,6 +1,9 @@
+import tkinter
 from tkinter import *
+from tkinter import ttk
 
 import customtkinter
+from tkcalendar import DateEntry
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
@@ -9,15 +12,172 @@ main = Tk()
 main.title('Labmate')
 main.geometry('1366x760')
 main.iconbitmap('')
-main.config(highlightthickness=2, bg='#4f82ac')
+main.config(highlightthickness=2, bg='black')
+
 
 
 # bg= PhotoImage(file='lovelight.png')
+
+
+# frame1 ==========================QC FRAME
+bench = ["Endoserology", "Haematology", "Chemistry"]
+
+endo_machine = ["Unicel DXI", "Access", "Abbott"]
+chem_machine = ["AU 680", "BIORAD DJ"]
+haema_machine = ["Sysmex XN550", "Sysmex XN31", "Biorad DM", "BD Fascount", "Sebia"]
+
+endo_analyte = ['Free t4', 'TSH', 'free t3']
+au_analyte = ['Na', 'K', 'Cl']
+dj_analyte = ['HBA1C']
+xn550_31 = ['FBC']
+hb_analyte = ['Hb Elect']
+bdfasc= ['CD4']
+day= ['AM', 'PM']
+
+
+
+def pick_machine(e):
+    if bench_combobox.get() == "Endoserology":
+        machine_combobox.config(value=endo_machine)
+
+def pick_analyte(e):
+    if machine_combobox.get() == "Unicel DXI":
+        analyte_combobox.config(value=endo_analyte)
+
+def add():
+    pass
+
+
+
 
 def show_frame1():
     hide_frames()
     frame1.pack(fill='both', expand=1)
 
+    bench_combobox = customtkinter.CTkComboBox(frame1, values=bench)
+    bench_combobox.grid(row=0, column=1, padx=5, sticky= 'nsew')
+    bench_combobox.bind("<<ComboboxSelected>>", pick_machine)
+
+    machine_combobox = customtkinter.CTkComboBox(frame1, values=endo_machine)
+    machine_combobox.grid(row=0, column=2, padx=5, sticky= 'nswe')
+    machine_combobox.bind("<<ComboboxSelected>>",pick_analyte)
+
+    analyte_combobox = customtkinter.CTkComboBox(frame1, values=endo_analyte)
+    analyte_combobox.grid(row=0, column=3, padx=5, columnspan=4, sticky= 'nsew')
+    analyte_combobox.bind('<<ComboboxSelected>>',pick_machine)
+
+    day_combo= customtkinter.CTkComboBox(frame1, values=day,width=90,).grid(row=0, column=8, padx=5,sticky='e')
+
+
+    users = (
+        'Fortune Nwosu',
+        'Amadi Uche',
+        'Kenneth Michelle',
+        'Osaigbovo Ebehrimen',
+        'Maryann',
+        'Eziebe CLaribel',
+        'Oloko Olorunfemi',
+        'Ndukwe Ruth',
+        'Enebeli Gerald',
+        'Trainee')
+
+    combx = customtkinter.CTkComboBox(frame1, values=users, width=30)
+    combx.grid(row=0, column=9, sticky= 'nsew')
+
+
+
+
+    trend_l = customtkinter.CTkCheckBox(frame1, text="Trend", text_font=("Helvetica", -16))
+    trend_l.grid(row=2, column=0, pady=20, padx=10, ipadx=27)
+
+    bias_l = customtkinter.CTkCheckBox(frame1, text="Bias", text_font=("Helvetica", -16))
+    bias_l.grid(row=3, column=0, pady=10, padx=10, ipadx=31)
+
+    shift_l = customtkinter.CTkCheckBox(frame1, text="Shift", text_font=("Helvetica", -16))
+    shift_l.grid(row=4, column=0, pady=20, padx=10, ipadx=31)
+
+    imprecision_l = customtkinter.CTkCheckBox(frame1, text="Imprecision", text_font=("Helvetica", -16))
+    imprecision_l.grid(row=5, column=0, pady=10, padx=10, ipadx=8)
+
+    random_l = customtkinter.CTkCheckBox(frame1, text="Random Error", text_font=("Helvetica", -16))
+    random_l.grid(row=6, column=0, pady=10, padx=0)
+
+    qc_label = customtkinter.CTkLabel(frame1,text="Document root cause, troubleshooting and action taken(Where applicable)",
+                                      text_font=("Helvetica", -13))  # <- custom tuple-color
+    qc_label.grid(row=2, column=1, sticky='sw')
+
+    add_items_label = customtkinter.CTkLabel(frame1,text="Items put in use",
+                                      text_font=("Helvetica", -13))  # <- custom tuple-color
+    add_items_label.grid(row=2, column=9, sticky='se')
+
+    add_entry = Text(frame1,width=20, height=6, bg='grey', fg='white')
+    add_entry.grid(row=1, column=9, rowspan=5, padx=6)
+
+    qc_entry = Text(frame1,width=128, height=6, bg='grey', fg='white')
+    qc_entry.grid(row=1, column=1, columnspan=8,rowspan=5)
+
+    qc_label2 = customtkinter.CTkLabel(frame1, text= "Acceptable",text_font=("Helvetica", -16)).grid(row=6, column=1,sticky='w')
+
+
+    eqa_no_entry= customtkinter.CTkEntry(frame1, width=10, placeholder_text="EQA CYCLE/SAMPLE USED TO VALIDATE")
+    eqa_no_entry.grid(row=5, column=1, sticky='we',ipadx=100)
+
+    add_bt = customtkinter.CTkButton(frame1,text="ADD",  command= add)
+    add_bt.grid(row=6, column=8)
+
+
+    qc_switch = customtkinter.CTkSwitch(frame1,text="QC PENDING")
+    qc_switch.grid(row=5, column=8)
+
+    sel= StringVar()
+
+    DateEntry(frame1, selectmode='day', date_pattern="dd-mm-yyyy").grid(row=0, column=0)
+
+    var1 = tkinter.IntVar(value=0)
+
+
+    eqa_radio1 = customtkinter.CTkRadioButton(master=frame1, variable=var1,
+                                              value=0, text= "yes",text_font=("Helvetica", -16))
+    eqa_radio1.grid(row=6, column=1, ipadx=65)
+
+    eqa_radio2 = customtkinter.CTkRadioButton(frame1,variable=var1,value=1,text= "no",text_font=("Helvetica", -16))
+    eqa_radio2.grid(row=6, column=1)
+    #treeview
+
+    # ntreeview styling
+    style=ttk.Style()
+    style.theme_use('alt')
+    style.configure('Treeview.Heading', background='black', foreground='white')
+    style.configure('Treeview',fieldbackground='grey')
+    style.configure('DateEntry', fieldbackground='#363636', foreground='white')
+
+    #style.map('Treeview.Heading', background=[('selected','green')])
+
+
+    trv = ttk.Treeview(frame1, columns=(1, 2, 3, 4, 5, 6), height=15, show='headings')
+    trv.grid(row=8, column=0, rowspan=6, columnspan=10, padx=10, sticky='nswe')
+
+    # headings
+    trv.heading(1, text="Date")
+    trv.heading(2, text="User")
+    trv.heading(3, text="Put in-use")
+    trv.heading(4, text="DQC Problem")
+    trv.heading(5, text="QC Management")
+    trv.heading(6, text="Machine")
+
+    # scrollbar
+    scrollbary = Scrollbar(frame1, orient=VERTICAL)
+    scrollbary.grid(row=8, column=9, rowspan=6, ipady=8,sticky='ens')
+    scrollbary.configure(command=trv.yview())
+    trv.configure(yscrollcommand=scrollbary.set)
+
+    update_bt = customtkinter.CTkButton(frame1, text="Update", command=add)
+    update_bt.grid(row=14, column=2, pady=10)
+
+    del_bt = customtkinter.CTkButton(frame1,text="Delete", command=add)
+    del_bt.grid(row=14, column=8, padx=5)
+
+    blaq= customtkinter.CTkLabel(frame1, text='❤ &💡', text_font=("Chiller", -15)).grid(row=15, column=9, padx=15,pady=30, sticky='s')
 
 def show_frame2():
     hide_frames()
@@ -40,11 +200,14 @@ def show_frame5():
 
 
 def show_frame6():
-    frame.tkraise()
+    hide_frames()
+    frame6.pack(fill='both', expand=1)
 
 
 def show_frame7():
-    wrapper1.tkraise()
+    hide_frames()
+    frame7.pack(fill='both', expand=1)
+
 
 def hide_frames():
     frame1.pack_forget()
@@ -52,9 +215,12 @@ def hide_frames():
     frame3.pack_forget()
     frame4.pack_forget()
     frame5.pack_forget()
+    frame6.pack_forget()
+    frame7.pack_forget()
 
 
-frame1 = Frame(main, width=600, height=500, bg='blue')
+# frame1 ==========================QC FRAME
+frame1 = Frame(main, width=600, height=500, bg='black')
 
 frame2 = Frame(main, width=600, height=500, bg='green')
 
@@ -63,6 +229,10 @@ frame3 = Frame(main, width=600, height=500, bg='yellow')
 frame4 = Frame(main, width=600, height=500, bg='black')
 
 frame5 = Frame(main, width=600, height=500, bg='white')
+
+frame6 = Frame(main, width=600, height=500, bg='white')
+
+frame7 = Frame(main, width=600, height=500, bg='white')
 
 '''
 def clear():
